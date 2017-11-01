0b67f8dbd6f985a08ee45e1a611cb99396877673
iamoatil <iamoatil@163.com>
2017-11-01 18:13:33 +0800

    添加文件夹拷贝功能

---
+++ Trunk/Trunk/Tools/CopyDll/CopyDll/CheckTimeTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+/* ==============================================================================
+* Description：CheckTimeTest  
+* Author     ：litao
+* Create Date：2017/11/1 16:18:18
+* ==============================================================================*/
+
+namespace CopyDll
+{
+    /// <summary>
+    /// CheckTimeTest
+    /// </summary>
+    class CheckTimeTest
+    {
+        public void ExistCheck()
+        {
+            string sourceDir = @"d:\SourceFiles\";
+            string targetDir = @"d:\TargetFiles\";
+            Stopwatch sw=new Stopwatch();
+            long lastTime = 0;
+            long curTime = 0;
+            long elapsedTime = 0;
+            sw.Start();
+
+            FileDirectory sourceDirectory = new FileDirectory(sourceDir,"*.*",SearchOption.AllDirectories);
+            
+            curTime = sw.ElapsedMilliseconds;
+            elapsedTime = curTime - lastTime;
+            lastTime = curTime;
+            Console.WriteLine("elapsedTime:" + elapsedTime);
+
+            FileDirectory targetDirectory = new FileDirectory(targetDir, "*.*", SearchOption.AllDirectories);
+            curTime = sw.ElapsedMilliseconds;
+            elapsedTime = curTime - lastTime;
+            lastTime = curTime;
+            Console.WriteLine("elapsedTime:" + elapsedTime);
+
+            FileDirectory.CopyIfNewest(sourceDirectory,targetDirectory);
+
+            curTime = sw.ElapsedMilliseconds;
+            elapsedTime = curTime - lastTime;
+            lastTime = curTime;
+            Console.WriteLine("elapsedTime:" + elapsedTime);
+        }
+    }
+}
---
+++ Trunk/Trunk/Tools/CopyDll/CopyDll/FileDirectory.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* ==============================================================================
+* Description：DllDirectory  
+* Author     ：litao
+* Create Date：2017/11/1 17:16:44
+* ==============================================================================*/
+
+namespace CopyDll
+{
+    /// <summary>
+    /// FileDirectory
+    /// </summary>
+    class FileDirectory
+    {
+        public FileDirectory(string dirPath, string searchPattern, SearchOption searchOption)
+        {
+            if (!Directory.Exists(dirPath))
+            {
+                Directory.CreateDirectory(dirPath);
+            }
+            _filePaths = Directory.GetFiles(dirPath, searchPattern, searchOption).ToList();
+            _dirPath = dirPath;
+        }
+
+        private readonly string _dirPath;
+        private readonly List<string> _filePaths;
+
+        /// <summary>
+        /// 目录拷贝
+        /// </summary>
+        /// <param name="sourceDir"></param>
+        /// <param name="targetDir"></param>
+        public static void Copy(FileDirectory sourceDir, FileDirectory targetDir)
+        {
+            foreach (string path in sourceDir._filePaths)
+            {
+                string fakeTargetPath = path.Replace(sourceDir._dirPath, targetDir._dirPath);
+                bool isExisted=targetDir._filePaths.Contains(fakeTargetPath);
+                if (!isExisted)
+                {
+                    string dirName = Path.GetDirectoryName(fakeTargetPath);
+                    if (!Directory.Exists(dirName))
+                    {
+                        Directory.CreateDirectory(dirName);
+                    }
+                    File.Copy(path,fakeTargetPath);
+                }
+            }
+        }
+        
+        /// <summary>
+        /// 如果最新则拷贝
+        /// </summary>
+        /// <param name="sourceDir"></param>
+        /// <param name="targetDir"></param>
+        public static void CopyIfNewest(FileDirectory sourceDir, FileDirectory targetDir)
+        {
+            foreach (string path in sourceDir._filePaths)
+            {
+                string fakeTargetPath = path.Replace(sourceDir._dirPath, targetDir._dirPath);
+                bool isExisted = targetDir._filePaths.Contains(fakeTargetPath);
+                if (!isExisted)
+                {
+                    string dirName = Path.GetDirectoryName(fakeTargetPath);
+                    if (!Directory.Exists(dirName))
+                    {
+                        Directory.CreateDirectory(dirName);
+                    }
+                    File.Copy(path, fakeTargetPath);
+                }
+                else
+                {
+                    DateTime targetFileTime=File.GetLastWriteTime(fakeTargetPath);
+                    DateTime sourceFileTime = File.GetLastWriteTime(path);
+
+                    if (sourceFileTime > targetFileTime)
+                    {
+                        File.Copy(path, fakeTargetPath,true);
+                    }
+                }
+            }
+        }
+    }
+}
