dd09a92fc9e90eb72833a8194b6987c170dbd978
iamoatil <iamoatil@163.com>
2017-11-13 15:44:52 +0800

    ProgressBar样式添加

---
+++ Trunk/Trunk/Source/21.Presentation/Theme/XLY.SF.Project.Themes/CustromControl/ProgressBar/ProgressBarWithCorner.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+/* ==============================================================================
+* Description：ProgressBar  
+* Author     ：litao
+* Create Date：2017/11/13 14:31:46
+* ==============================================================================*/
+
+namespace XLY.SF.Project.Themes.CustromControl
+{
+    public class ProgressBarWithCorner: ProgressBar
+    {
+        static ProgressBarWithCorner()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ProgressBarWithCorner), new FrameworkPropertyMetadata(typeof(ProgressBarWithCorner)));
+        }
+
+        /// <summary>
+        ///  圆角的大小
+        /// </summary>
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(ProgressBarWithCorner), new PropertyMetadata(new CornerRadius()));
+
+        /// <summary>
+        /// 背景色的透明度
+        /// </summary>
+        public double BackgroundOpacity
+        {
+            get { return (double)GetValue(BackgroundOpacityProperty); }
+            set { SetValue(BackgroundOpacityProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for BackgroundOpacity.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BackgroundOpacityProperty =
+            DependencyProperty.Register("BackgroundOpacity", typeof(double), typeof(ProgressBarWithCorner), new PropertyMetadata(1.0));
+
+    }
+}
