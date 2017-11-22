be383ceb9c11cbcf60fa89f933f2c93582d02ad7
iamoatil <iamoatil@163.com>
2017-11-23 00:33:57 +0800

    迁入测试代码

--- Trunk/Trunk/Source/41.Test/XLY.SF.WpfTest/LiTao/EarlyWarningTest/Md5ConfigFileDirUnitTest.cs
+++ Trunk/Trunk/Source/41.Test/XLY.SF.WpfTest/LiTao/EarlyWarningTest/Md5ConfigFileDirUnitTest.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using XLY.SF.Project.EarlyWarningView;
 
 namespace EarlyWarningTest
 {
@@ -8,8 +11,28 @@ namespace EarlyWarningTest
         [TestMethod]
         public void GetAllData()
         {
+            string path = @"TestFiles\Md5File.xml";
+            if (!File.Exists(path))
+            {
+                string content = @"<FileMd5Collection >"
+                                + @" <Category Name = ""涉黄"" >"
+                                + @"     <FileMd5 Value = ""acb23tyrt7823rty1"" />"
+                                + @"     <FileMd5 Value = ""acb23tyrt7823rty2"" />"
+                                + @" </Category>"
+                                + @" <Category Name = ""涉毒"" >"
+                                + @"     <FileMd5 Value = ""acb23tyrt7823rty3"" />"
+                                + @" </Category >"
+                                + @"</FileMd5Collection>";
+                string directoty = Path.GetDirectoryName(path);
+                if (!Directory.Exists(directoty))
+                {
+                    Directory.CreateDirectory(directoty);
+                }
+                File.WriteAllText(path, content);
+            }
+
             Md5ConfigFileDir dir = new Md5ConfigFileDir();
-            dir.Initialize(@"D:\Test\");
+            dir.Initialize(@"TestFiles\");
             List<SensitiveData> ls = dir.GetAllData();
         }
     }
--- Trunk/Trunk/Source/41.Test/XLY.SF.WpfTest/LiTao/EarlyWarningTest/UnitTest1.cs
+++ Trunk/Trunk/Source/41.Test/XLY.SF.WpfTest/LiTao/EarlyWarningTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 
 namespace EarlyWarningTest
 {
@@ -9,16 +10,9 @@ namespace EarlyWarningTest
         [TestMethod]
         public void TestMethod1()
         {
+            
+            
+
         }
     }
-
-    <FileMd5Collection>
- <Category Name = "涉黄" >
-    < FileMd5 Value="acb23tyrt7823rty1"/>
-    <FileMd5 Value = "acb23tyrt7823rty2" />
- </ Category >
- < Category Name = "涉毒">
-    <FileMd5 Value = "acb23tyrt7823rty3" />
- </ Category >
-</ FileMd5Collection >
 }
