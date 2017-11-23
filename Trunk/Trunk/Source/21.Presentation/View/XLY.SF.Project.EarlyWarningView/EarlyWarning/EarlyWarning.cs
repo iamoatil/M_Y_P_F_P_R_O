9cea77333f5720f6e9cb232e7a0dcfb0ffe73400
iamoatil <iamoatil@163.com>
2017-11-23 11:08:57 +0800

    使用readonly static string代替硬的字符串

--- Trunk/Trunk/Source/21.Presentation/View/XLY.SF.Project.EarlyWarningView/EarlyWarning/EarlyWarning.cs
+++ Trunk/Trunk/Source/21.Presentation/View/XLY.SF.Project.EarlyWarningView/EarlyWarning/EarlyWarning.cs
@@ -11,10 +11,11 @@ namespace XLY.SF.Project.EarlyWarningView
 {
     class EarlyWarning
     {
-        private static string _fileMd5Name = "FileMd5";
-        private static string _netAddressName = "NetAddress";
-        private static string _keyWordName = "KeyWord";
-        private static string _appName = "App";
+        private readonly static string _fileMd5Name = "FileMd5";
+        private readonly static string _netAddressName = "NetAddress";
+        private readonly static string _keyWordName = "KeyWord";
+        private readonly static string _appName = "App";
+
         private Dictionary<string, IDetection> _detectionDic = new Dictionary<string, IDetection>();
 
         /// <summary>
@@ -38,14 +39,29 @@ namespace XLY.SF.Project.EarlyWarningView
             _baseDir = Path.GetFullPath(@"EarlyWarning\");
             //Md5敏感初始化
             Md5ConfigFileDir md5ConfigFileDir = new Md5ConfigFileDir();
-            md5ConfigFileDir.Initialize(_baseDir+@"\FilMd5Config\");
+            md5ConfigFileDir.Initialize(string.Format(@"{0}{1}Config\", _baseDir, _fileMd5Name));
             Md5Detection md5Detection = new Md5Detection();
             md5Detection.Initialize(md5ConfigFileDir.GetAllData());
             _detectionDic.Add(_fileMd5Name, md5Detection);
-            //网址敏感初始化
             //应用敏感初始化
+            AppNameConfigFileDir appNameConfigFileDir = new AppNameConfigFileDir();
+            appNameConfigFileDir.Initialize(string.Format(@"{0}{1}Config\", _baseDir, _appName));
+            AppNameDetection appNameDetection = new AppNameDetection();
+            appNameDetection.Initialize(appNameConfigFileDir.GetAllData());
+            _detectionDic.Add(_appName, appNameDetection);
             //关键字敏感初始化
-            
+            KeyWordConfigFileDir keyWordConfigFileDir = new KeyWordConfigFileDir();
+            keyWordConfigFileDir.Initialize(string.Format(@"{0}{1}Config\", _baseDir, _keyWordName));
+            KeyWordDetection keyWordDetection = new KeyWordDetection();
+            keyWordDetection.Initialize(keyWordConfigFileDir.GetAllData());
+            _detectionDic.Add(_keyWordName, keyWordDetection);
+            //网址敏感初始化
+            NetAddressConfigFileDir netAddressConfigFileDir = new NetAddressConfigFileDir();
+            netAddressConfigFileDir.Initialize(string.Format(@"{0}{1}Config\", _baseDir, _netAddressName));
+            NetAddressDetection netAddressDetection = new NetAddressDetection();
+            netAddressDetection.Initialize(netAddressConfigFileDir.GetAllData());
+            _detectionDic.Add(_netAddressName, netAddressDetection);
+
             _isInitialized = true;
             return _isInitialized;
         }
