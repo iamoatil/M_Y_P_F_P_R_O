da4252aadcfd37dbf30be8980d3ce0c386c10dc0
iamoatil <iamoatil@163.com>
2017-11-23 09:20:52 +0800

    修改打开XLY.Shell.Sln批处理的文件

--- Trunk/Trunk/Source/OpenShell.bat
+++ Trunk/Trunk/Source/OpenShell.bat
@@ -1,6 +1,7 @@
 set curDir = %~dp0 
-set vs2017="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe"
-call %vs2017% %curDir%23.Shell\XLY.SF.Shell\XLY.SF.Shell.sln
+::set vs2017="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe"
+::call %vs2017% %curDir%23.Shell\XLY.SF.Shell\XLY.SF.Shell.sln
+call %curDir%23.Shell\XLY.SF.Shell\XLY.SF.Shell.sln
 
 
 
