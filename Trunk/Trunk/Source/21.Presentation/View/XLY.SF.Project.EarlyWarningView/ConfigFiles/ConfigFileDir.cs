d18c8b8cc0ed434df4efbf55dfe1ad21a3c509e2
iamoatil <iamoatil@163.com>
2017-11-25 13:39:12 +0800

    把配置文件结构修改了一下

---
+++ Trunk/Trunk/Source/21.Presentation/View/XLY.SF.Project.EarlyWarningView/ConfigFiles/ConfigFileDir.cs
@@ -0,0 +1,36 @@
+﻿/* ==============================================================================
+* Description：ConfigFileDir  
+*           此文件中定义了4个关于 ConfigFileDir 的类，他们都继承于 AbstractConfigFileDir 类
+*           以下左边的是右边的具体类型           
+*               AppNameConfigFileDir        -------》 AbstractConfigFileDir
+*               KeyWordConfigFileDir        -------》 AbstractConfigFileDir
+*               Md5ConfigFileDir            -------》 AbstractConfigFileDir
+*               NetAddressConfigFileDir     -------》 AbstractConfigFileDir
+*           
+
+* Author     ：litao
+* Create Date：2017/11/25 13:35:51
+* ==============================================================================*/
+
+namespace XLY.SF.Project.EarlyWarningView
+{
+    class AppNameConfigFileDir : AbstractConfigFileDir
+    {
+        protected override string RootName { get { return "AppNameCollection"; } }
+    }
+
+    class KeyWordConfigFileDir : AbstractConfigFileDir
+    {
+        protected override string RootName { get { return "KeyWordCollection"; } }
+    }
+
+    class Md5ConfigFileDir : AbstractConfigFileDir
+    {
+        protected override string RootName { get { return "FileMd5Collection"; } }
+    }
+
+    class NetAddressConfigFileDir : AbstractConfigFileDir
+    {
+        protected override string RootName { get { return "NetAddressCollection"; } }
+    }
+}
