e0d479069705f083e3a35309e29fdd5717926a9e
iamoatil <iamoatil@163.com>
2017-11-30 11:36:00 +0800

    修整到tfs22043

--- Trunk/Trunk/Source/11.Service/13.Core/XLY.SF.Project.Devices/DeviceManager/IOS/IOSDeviceManager.cs
+++ Trunk/Trunk/Source/11.Service/13.Core/XLY.SF.Project.Devices/DeviceManager/IOS/IOSDeviceManager.cs
@@ -106,8 +106,6 @@ namespace XLY.SF.Project.Devices
             //1.初始化相关参数
             InitCopyUserData(device, 100, asyn);
 
-            Directory.SetCurrentDirectory(Path.Combine(Environment.CurrentDirectory, @"Lib\vcdllX64\IphoneDevices"));
-
             //2.文件拷贝
             var res = IOSDeviceCoreDll.CopyUserData(targetPath, device.ID, _CopyUserDataCallback);
 
@@ -141,8 +139,6 @@ namespace XLY.SF.Project.Devices
         {
             //1.初始化相关参数
             InitCopyUserData(device, 100, asyn);
-
-            Directory.SetCurrentDirectory(Path.Combine(Environment.CurrentDirectory, @"Lib\vcdllX64\IphoneDevices"));
 
             //2.文件拷贝
             var res = IOSDeviceCoreDll.CopyUserDataPWD(targetPath, device.ID, CopyUserDataCallback, (b) =>
@@ -346,8 +342,6 @@ namespace XLY.SF.Project.Devices
         /// <returns></returns>
         public List<AppEntity> FindInstalledApp(Device device)
         {
-            Directory.SetCurrentDirectory(Path.Combine(Environment.CurrentDirectory, @"Lib\vcdllX64\IphoneDevices"));
-
             var appPointer = IntPtr.Zero;
             uint execResultCode = IOSDeviceCoreDll.GetALLInstalledApp(device.ID, ref appPointer);
             var appEntities = new List<AppEntity>();
